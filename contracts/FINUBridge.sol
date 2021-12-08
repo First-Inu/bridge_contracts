187d9af64cd640b92b31e74117e4b9cfe67fafa2
Perfect-SoftwareEngineer <67376797+Perfect-SoftwareEngineer@users.noreply.github.com>
2021-12-08 23:22:49 +0800

    feat(contract): add swapId for verification

--- contracts/FINUBridge.sol
+++ contracts/FINUBridge.sol
@@ -2,9 +2,13 @@
 pragma solidity ^0.8.3;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract FINUBridge is Ownable {
+    using SafeMath for uint256;
+
     address finuTokenContractAddress;
+    uint256 public swapIdPointer;
 
     constructor(address _finuTokenContractAddress) {
         finuTokenContractAddress = _finuTokenContractAddress;
@@ -14,9 +18,17 @@ contract FINUBridge is Ownable {
         finuTokenContractAddress = _finuTokenContractAddress;
     }
 
-    function lockToken(uint256 amount) external {
+    function getCurrentSwapId() public view returns(uint){
+        uint swapId = swapIdPointer;
+        return swapId;
+    }
+
+    function lockToken(uint256 amount) external returns(uint){
         require(IERC20(finuTokenContractAddress).balanceOf(msg.sender) >= amount, "FINUBridge: not enough balance");
         require(IERC20(finuTokenContractAddress).allowance(msg.sender, address(this)) >= amount, "FINUBridge: not allowed");
         IERC20(finuTokenContractAddress).transferFrom(msg.sender, address(this), amount);
+        swapIdPointer = swapIdPointer.add(1);
+        uint swapId = swapIdPointer;
+        return swapId;
     }
 }
